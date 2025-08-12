6d575ce0b5d226c991bf04ed740894cfee57cfab
Geoffrey Irving <irving@naml.us>
2025-08-12 21:32:43 +0100

    Facts about tanh

--- Ray/Misc/Bound.lean
+++ Ray/Misc/Bound.lean
@@ -1,7 +1,11 @@
+import Mathlib.Analysis.InnerProductSpace.Basic
 import Mathlib.Analysis.Normed.Group.Basic
+import Mathlib.Data.Complex.Basic
+import Mathlib.Data.Complex.Trigonometric
 
 /-!
 ## Register some bound lemmas
 -/
 
-attribute [bound] norm_add_le mul_lt_of_lt_one_left
+attribute [bound] norm_add_le mul_lt_of_lt_one_left Complex.normSq_nonneg norm_inner_le_norm
+  Units.norm_pos Real.cosh_pos
